284d83b08c7cea70a1b053fb059c190148f23986
kusha12 <kuashasarker098765@gmail.com>
2026-03-31 18:52:08 +0600

    my banner complet

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,22 +1,43 @@
 import React from "react";
-
+import cartImg from "../../assets/assets/Play.png";
+import bannerImg from "../../assets/assets/banner.png";
 const Banner = () => {
   return (
-    <div className='hero bg-base-200 min-h-screen'>
-      <div className='hero-content flex-col lg:flex-row'>
-        <img
-          src='https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp'
-          className='max-w-sm rounded-lg shadow-2xl'
-        />
-        <div>
-          <h1 className='text-5xl font-bold'>Box Office News!</h1>
-          <p className='py-6'>
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className='btn btn-primary'>Get Started</button>
+    <div className='flex justify-around w-11/12 mx-auto'>
+      <div className='pt-32 '>
+        <button className='text-[#2b16cf] py-2 px-14 bg-gray-200 rounded-3xl '>
+          New: AI-Powered Tools Available
+        </button>
+
+        <div className='gap-3'>
+          <h1 className='font-bold text-7xl pt-6 text-[#101727]'>
+            Supercharge Your <br />
+            Digital Workflow
+          </h1>
         </div>
+        <p className='text-[#627382] pt-4'>
+          Access premium AI tools, design assets, templates, and productivity{" "}
+          <br />
+          software—all in one place. Start creating faster today. <br /> Explore
+          Products
+        </p>
+        <div className='flex pt-5 gap-4'>
+          <div>
+            <button className='py-3 px-4 bg-linear-65 from-purple-500 to-pink-500 rounded-3xl text-white  font-bold'>
+              Explore Products
+            </button>
+          </div>
+          <div className=''>
+            <button className='flex py-3 px-4 text-[#2b16cf] border-2 border-[#2b16cf] font-bold rounded-3xl gap-2'>
+              <img src={cartImg} alt='' />
+              Watch Demo
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className='pt-20'>
+        <img src={bannerImg} alt='' />
       </div>
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,32 +2,36 @@ import React from "react";
 import cartImg from "../../assets/assets/products/shopping-cart.png";
 const Navbar = () => {
   return (
-    <div className='flex justify-around h-20  items-center border border-gray-300'>
-      <h1 className='text-5xl font-bold'>
-        <span className=' text-[#2b16cf]'>DigiTools</span>
-      </h1>
+    <nav className='   h-20 mx-auto  border border-gray-300'>
+      <div className='  mt-3'>
+        <div className='flex justify-around   items-center  '>
+          <h1 className='text-5xl font-bold'>
+            <span className=' text-[#2b16cf]'>DigiTools</span>
+          </h1>
 
-      <div className='flex items-center'>
-        <ul className='flex gap-6 font-bold'>
-          <li>Products</li>
-          <li>Features</li>
-          <li>Pricing</li>
-          <li>Testimonials</li>
-          <li>FAQ</li>
-        </ul>
-      </div>
-      <div className='flex gap-4 items-center'>
-        <div>
-          <img src={cartImg} alt='' />
+          <div className='flex items-center'>
+            <ul className='flex gap-6 font-bold'>
+              <li>Products</li>
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Testimonials</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+          <div className='flex gap-4 items-center'>
+            <div>
+              <img src={cartImg} alt='' />
+            </div>
+            <h2 className='font-bold'>Login</h2>
+            <div>
+              <button className='py-2 px-3 bg-linear-65 from-purple-500 to-pink-500 rounded-3xl text-white'>
+                Get Started
+              </button>
+            </div>
+          </div>
         </div>
-        <h2 className='font-bold'>Login</h2>
-        <div>
-          <button className='py-2 px-3 bg-linear-65 from-purple-500 to-pink-500 rounded-3xl text-white'>
-            Get Started
-          </button>
-        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
