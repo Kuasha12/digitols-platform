ae095915ecaf7e6f7cd15181c9c85e0e6f5a40cf
kusha12 <kuashasarker098765@gmail.com>
2026-03-31 13:06:15 +0600

    my navbar complet

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,75 +1,31 @@
 import React from "react";
-
+import cartImg from "../../assets/assets/products/shopping-cart.png";
 const Navbar = () => {
   return (
-    <div className='navbar bg-base-100 shadow-sm'>
-      <div className='navbar-start'>
-        <div className='dropdown'>
-          <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'>
-              {" "}
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M4 6h16M4 12h8m-8 6h16'
-              />{" "}
-            </svg>
-          </div>
-          <ul
-            tabIndex='-1'
-            className='menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow'>
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className='p-2'>
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <a className='btn btn-ghost text-xl'>daisyUI</a>
-      </div>
-      <div className='navbar-center hidden lg:flex'>
-        <ul className='menu menu-horizontal px-1'>
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className='p-2 bg-base-100 w-40 z-1'>
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+    <div className='flex justify-around h-20  items-center border border-gray-300'>
+      <h1 className='text-5xl font-bold'>
+        <span className=' text-[#2b16cf]'>DigiTools</span>
+      </h1>
+
+      <div className='flex items-center'>
+        <ul className='flex gap-6 font-bold'>
+          <li>Products</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Testimonials</li>
+          <li>FAQ</li>
         </ul>
       </div>
-      <div className='navbar-end'>
-        <a className='btn'>Button</a>
+      <div className='flex gap-4 items-center'>
+        <div>
+          <img src={cartImg} alt='' />
+        </div>
+        <h2 className='font-bold'>Login</h2>
+        <div>
+          <button className='py-2 px-3 bg-linear-65 from-purple-500 to-pink-500 rounded-3xl text-white'>
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
   );
