62df3a3ea87342546f7ed0466446bdad9cee9967
kusha12 <kuashasarker098765@gmail.com>
2026-03-31 21:16:56 +0600

    reating complet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/banner/Banner";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
+import Reating from "./components/reating/Reating";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Reating></Reating>
     </>
   );
 }
---
+++ src/components/reating/Reating.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Reating = () => {
+  return (
+    <div className='pt-20'>
+      <div className='bg-linear-65 from-purple-500 to-pink-500  '>
+        <div className='flex  justify-center items-center w-[1600px] h-[247px] '>
+          <div className='w-[398.67px]   justify-center items-center '>
+            <h1 className='flex justify-center items-center font-bold text-white text-5xl'>
+              50K+
+            </h1>
+            <p className='flex justify-center items-center font-bold text-white pt-4'>
+              Active Users
+            </p>
+          </div>
+          <div className='w-[398.67px]  justify-center items-center border-l-2 border-l-gray-200'>
+            <h1 className='flex justify-center items-center font-bold text-white text-5xl'>
+              200+
+            </h1>
+            <p className='flex justify-center items-center font-bold text-white pt-4'>
+              Premium Tools
+            </p>
+          </div>
+          <div className='w-[398.67px] justify-center items-center border-l-2 border-l-gray-200'>
+            <h1 className='flex justify-center items-center font-bold text-white text-5xl'>
+              4.9
+            </h1>
+            <p className='flex justify-center items-center font-bold text-white pt-4'>
+              Rating
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reating;
